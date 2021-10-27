71cbc0b67c8d731833e2f08cf5916a3fcb2704d3
C.Y. Tseng <diella.tseng@gmail.com>
2021-10-27 11:44:33 +0200

    create basic app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const helmet = require("helmet");
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+const userRoutes = require('./routes/user')
+const postRoutes = require('./routes/post')
+
+// Loads environment variables from the .env file into process.env
+require('dotenv').config();
+
+// Enable all CORS requests
+app.use(cors());
+
+// Parsing incoming requests with JSON payloads by using the express embedded functions
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Secure HTTP headers by using helmet
+app.use(helmet());
+
+// app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/user', userRoutes);
+app.use('/api/post', postRoutes);
+
+module.exports = app;
