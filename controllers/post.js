a17e1a7f829c65702aad22d7258011980eb5b3f1
C.Y. Tseng <diella.tseng@gmail.com>
2021-10-28 14:32:49 +0200

    render posts from db

--- controllers/post.js
+++ controllers/post.js
@@ -3,13 +3,22 @@ const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
-
 // Get all posts //
-exports.getPost = async (req, res, next) => {
+exports.getAllPosts = async (req, res, next) => {
     const posts = await prisma.post.findMany({
         orderBy: {
             created_at: 'desc',
-        }
+        },
+        include:{
+            comment: true,
+            _count: {
+                select: {
+                    comment: true,
+                    like: true
+                }
+            }
+        },
+        
     })
     .then(posts => res.status(200).json(posts))
     .catch(error => res.status(404).json({ message: error.message }));
--- routes/post.js
+++ routes/post.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const postCtrl = require('../controllers/post');
 
-router.get('/', postCtrl.getPost);
+router.get('/', postCtrl.getAllPosts);
 
 module.exports = router;
 
