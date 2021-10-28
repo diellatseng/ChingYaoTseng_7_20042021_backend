22da0a18491d986b839918ec1905d59d9ccd4a07
C.Y. Tseng <diella.tseng@gmail.com>
2021-10-28 11:09:25 +0200

    add getAll post ctrl

--- controllers/post.js
+++ controllers/post.js
@@ -6,6 +6,11 @@ const prisma = new PrismaClient()
 
 // Get all posts //
 exports.getPost = async (req, res, next) => {
-    const posts = await prisma.post.findMany()
-    console.log(posts)
+    const posts = await prisma.post.findMany({
+        orderBy: {
+            created_at: 'desc',
+        }
+    })
+    .then(posts => res.status(200).json(posts))
+    .catch(error => res.status(404).json({ message: error.message }));
 };
