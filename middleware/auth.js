f22abc37391f340b7b0517af915d1c97b29a4563
C.Y. Tseng <diella.tseng@gmail.com>
2021-10-30 22:10:10 +0200

    add auth to get all posts

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,21 +3,17 @@ const connectdb = require('../connectdb.js');
 const mysql = require('mysql');
 
 module.exports = (req, res, next) => {
-        console.log(req.headers);
     try {
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, process.env.TOKEN);
         const userId = decodedToken.userId;
         let sqlInserts = [userId];
-        let sql = 'SELECT COUNT(id) FROM users WHERE id=?';
-        sql = mysql.format(sql, sqlInserts); 
-        connectdb.query(sql, function(err, result){
-            if (err) reject({error : 'Error!'});
-            if (result[0]['COUNT(id)'] !== 1) {
-                throw 'Invalid User ID';
-            } else {
-                next();
-            }
+        let sql = 'SELECT * FROM user WHERE id=?';
+        sql = mysql.format(sql, sqlInserts);
+        console.log(sql);
+        connectdb.query(sql, function(err, rows, fields){
+            if (err) throw err;
+            next();
         })
     } catch (error) {
         res.status(401).json({error: error})
--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
 const postCtrl = require('../controllers/post');
+
+const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config')
 
-router.get('/', postCtrl.getAllPosts);
+router.get('/', auth, postCtrl.getAllPosts);
 router.post('/', multer, postCtrl.createPost);
 
 
@@ -11,7 +13,6 @@ module.exports = router;
 
 
 // const sauceCtrl = require('../controllers/sauce');
-// // const auth = require('../middleware/auth');
 // // const multer = require('../middleware/multer-config')
 
 // // router.get('/', auth, sauceCtrl.getSauce);
